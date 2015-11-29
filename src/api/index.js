d9bc85a308a808e5b4f74906fa7b5b6f51b1b9ee
fredy8 <fredy.altamirano8@gmail.com>
2015-11-29 15:24:38 -0600

    fixed bug where index would give 401 when not logged in

--- src/api/index.js
+++ src/api/index.js
@@ -1,17 +1,17 @@
 import R from 'ramda';
 import express from 'express';
 import auth from './auth';
-import { getUser } from './auth/authorization';
 import projects from './projects';
 import profiles from './profiles';
 import errorHandlers from './errorHandlers';
 import serverName from './serverName';
+import db from '../database';
 
 const router = express.Router();
 
 router.use(auth);
 
-router.get('/', getUser, (req, res) => {
+router.get('/', (req, res) => {
   const root = {
     _rels: {
       self: serverName.api
@@ -19,19 +19,22 @@ router.get('/', getUser, (req, res) => {
   };
 
   if (req.username) {
-    root._rels.profile = serverName.api + `profiles/${req.user.id}`;
     root._rels.projects = serverName.api + 'projects';
-    
-    root._embedded = {
-      profile: R.pick(['username', 'name'], req.user)
-    };
-    
+
+    db.queryAsync('SELECT id, username, name FROM Users where username = $1', [req.username])
+    .then(({rows}) => {
+      root._rels.profile = serverName.api + `profiles/${rows[0].id}`;
+      root._embedded = {
+        profile: R.pick(['username', 'name'], rows[0])
+      };
+      res.json(root);
+    });
   } else {
     root._rels.login = serverName.api + 'login';
     root._rels.register = serverName.api + 'register';
+    res.json(root);
   }
-
-  res.json(root);
+  
 });
 
 router.use(projects);
